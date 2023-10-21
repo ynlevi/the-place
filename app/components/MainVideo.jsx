1420a5bba4b38cea69db99110055939b08270e21
Yonathan Levi <jonilevi@icloud.com>
2023-10-21 19:06:37 +0300

    added value of true videos config

--- app/components/MainVideo.jsx
+++ app/components/MainVideo.jsx
@@ -5,11 +5,11 @@ function MainVideo() {
         "https://res.cloudinary.com/dcb9wy3kf/video/upload/v1696493609/main-video.mp4"
       }
       className="h-screen w-screen object-cover -z-20 relative"
-      autoPlay
       playing={true}
-      muted={true}
       loop={true}
-      playsInline
+      muted={true}
+      autoPlay={true}
+      playsInline={true}
     />
   );
 }
